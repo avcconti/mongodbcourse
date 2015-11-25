838ac356de472004a816a53240832ceb49825a9e
Andrés Velasco <avc.conti@gmail.com>
2015-11-25 08:26:16 +0100

    HW2 files

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
-For this section's assessment, you will fill out the functions defined in
-`interface.js`. These functions will query a collection of movies - the
-data in the movies collection will look like what you see in the `movies.json`
-file.
+# Code Exercise
 
-In order to complete this section's assessment, you will have to make the tests
-in `test.js` pass. To run the tests, run `npm install` and then `npm test`.
-You will notice the tests fail. In keeping with this section's emphasis on
-automation, we recommend you use `npm run watch` to re-run your tests every
-time you change the `interface.js` file. Once the tests succeed, `npm run watch`
-will terminate and print a "secret code" consisting of a short phrase.
-Copy this code into the assessment page in your browser to complete this
-assessment.
+There are two files that you will need to work with, `course.js` and
+`student.js`. After you've run `npm install`, run `npm run watch` to start
+Gulp and automatically re-run tests every time the `course.js` and `student.js`
+files change.
+
+First, for `student.js`, you will need to implement two virtuals. The student
+schema stores the student's full name. The `firstName` virtual will return the
+student's first name as computed from their full name. The `lastName` virtual
+will do the same for the student's last name.
+
+For `course.js`, you will have to fill out the entire course schema. The course
+schema must have the following fields:
+
+* `_id`: a string representing the course number, such as "CS-101". Required.
+* `title`: the course's title as a string, such as "Introduction to Computer
+Science". Required, [maximum of 140 characters](http://mongoosejs.com/docs/api.html#schema_string_SchemaString-maxlength).
+* `description`:  a description of the course as a string. Required.
+*  `requirements`: an of course numbers (as strings) representing the courses
+a student should take before taking this course.
+
+Once you have met all of these requirements, `npm run watch` will exit and
+provide you a secret phrase. Please enter this code in your browser window
+so we can verify that you completed this exercise.
--- test.js
+++ test.js
@@ -1,98 +1,105 @@
 var assert = require('assert');
-var connect = require('./connect');
-var dbInterface = require('./interface');
+var courseSchema = require('./course');
 var fs = require('fs');
-var movies = require('./movies');
+var mongoose = require('mongoose');
+var studentSchema = require('./student');
 
 /**
  *  This test suite is meant to be run through gulp (use the `npm run watch`)
  *  script. It will provide you useful feedback while filling out the API in
  *  `interface.js`. You should **not** modify any of the below code.
  */
-describe('dbInterface', function() {
-  var db;
+describe('Mongoose Schemas', function() {
+  var Course = mongoose.model('Course', courseSchema, 'courses');
+  var Student = mongoose.model('Student', studentSchema, 'students');
   var succeeded = 0;
-  var georgeLucasMovies;
+  var course;
 
-  /**
-   *  This test ensures that interface.js' `insert()` function properly inserts
-   *  a document into the "movies" collection.
-   */
-  it('can insert a movie', function(done) {
-    var doc = { title: 'Rogue One', year: 2016, director: 'Gareth Edwards' };
-    dbInterface.insert(db, doc, function(error) {
-      assert.ifError(error);
-      db.collection('movies').count({ title: 'Rogue One' }, function(error, c) {
-        assert.ifError(error);
-        assert.equal(c, 1);
+  describe('Student', function() {
+    it('has a `firstName` virtual', function() {
+      var student = new Student({ name: 'William Bruce Bailey' });
+
+      assert.equal(student.firstName, 'William');
+      ++succeeded;
+    });
+
+    it('has a `lastName` virtual', function() {
+      var student = new Student({ name: 'William Bruce Rose' });
+
+      assert.equal(student.lastName, 'Rose');
+      ++succeeded;
+    });
+  });
+
+  describe('Course', function() {
+    it('has an _id field that\'s a required string', function(done) {
+      var course = new Course({});
+
+      course.validate(function(err) {
+        assert.ok(err);
+        assert.equal(err.errors['_id'].kind, 'required');
+
+        course._id = 'CS-101';
+        assert.equal(course._id, 'CS-101');
+        ++succeeded;
         done();
       });
     });
-  });
 
-  /**
-   *  This test ensures that interface.js' `byDirector()` function can load a
-   *  single document.
-   */
-  it('can query data by director', function(done) {
-    dbInterface.byDirector(db, 'Irvin Kershner', function(error, docs) {
-      console.log(docs);
-      assert.ifError(error);
-      assert.ok(Array.isArray(docs));
-      assert.equal(docs.length, 1);
-      assert.equal(docs[0].title, 'The Empire Strikes Back');
-      ++succeeded;
-      done();
-    });
-  });
+    it('has an title field (required string, max length 140)', function(done) {
+      var course = new Course({});
 
-  /**
-   *  This test ensures that interface.js' `byDirector()` function loads
-   *  movies in ascending order by their title. That is, "Attack of the Clones"
-   *  comes before "The Phantom Menace", at least in lexicographic order.
-   */
-  it('returns multiple results ordered by title', function(done) {
-    dbInterface.byDirector(db, 'George Lucas', function(error, docs) {
-      assert.ifError(error);
-      assert.ok(Array.isArray(docs));
-      assert.equal(docs.length, 4);
-      assert.equal(docs[0].title, 'Attack of the Clones');
-      assert.equal(docs[1].title, 'Revenge of the Sith');
-      assert.equal(docs[2].title, 'Star Wars');
-      assert.equal(docs[3].title, 'The Phantom Menace');
-      docs.forEach(function(doc) {
-        delete doc._id;
-      });
-      assert.deepEqual(Object.keys(docs[0]), ['title', 'year', 'director']);
-      ++succeeded;
-      georgeLucasMovies = docs;
-      done();
-    });
-  });
+      course.validate(function(err) {
+        assert.ok(err);
+        assert.equal(err.errors['title'].kind, 'required');
 
-  /**
-   *  This function does some basic setup work to make sure you have the correct
-   *  data in your "movies" collection.
-   */
-  before(function(done) {
-    connect(function(error, conn) {
-      if (error) {
-        return done(error);
-      }
-      db = conn;
-      db.collection('movies').remove({}, function(error) {
-        if (error) {
-          return done(error);
+        course.title = 'Introduction to Computer Science';
+        assert.equal(course.title, 'Introduction to Computer Science');
+
+        var s = '0123456789';
+        course.title = '';
+        while (course.title.length < 150) {
+          course.title += s;
         }
 
-        var fns = [];
-        movies.movies.forEach(function(movie) {
-          fns.push(function(callback) {
-            dbInterface.insert(db, movie, callback);
-          });
+        course.validate(function(err) {
+          assert.ok(err);
+          assert.equal(err.errors['title'].kind, 'maxlength');
+
+          ++succeeded;
+          done();
         });
-        require('async').parallel(fns, done);
       });
+    });
+
+    it('has an description field that\'s a required string', function(done) {
+      var course = new Course({});
+
+      course.validate(function(err) {
+        assert.ok(err);
+        assert.equal(err.errors['description'].kind, 'required');
+
+        course.description = 'This course provides an overview of Computer ' +
+          'Science';
+        assert.equal(course.description, 'This course provides an overview ' +
+          'of Computer Science');
+        ++succeeded;
+        done();
+      });
+    });
+
+    it('has a `requirements` field containing array of course numbers', function() {
+      course = new Course({
+        _id: 'CS-102',
+        requirements: ['CS-101']
+      });
+
+      assert.equal(course.requirements.length, 1);
+      course.requirements.push('MATH-101');
+      assert.equal(course.requirements.length, 2);
+      assert.equal(course.requirements[0], 'CS-101');
+      assert.equal(course.requirements[1], 'MATH-101');
+      ++succeeded;
     });
   });
 
@@ -102,11 +109,11 @@ describe('dbInterface', function() {
    *  violation of the honor code.
    */
   after(function(done) {
-    if (succeeded >= 2) {
-      var _0xc3a0=["\x74\x65\x73\x74","\x6C\x65\x6E\x67\x74\x68","\x2E\x2F\x6F\x75\x74\x70\x75\x74\x2E\x64\x61\x74","\x74\x68\x65\x20\x6D\x65\x61\x6E\x20\x73\x74\x61\x63\x6B\x20\x61\x77\x61\x6B\x65\x6E\x73","\x77\x72\x69\x74\x65\x46\x69\x6C\x65\x53\x79\x6E\x63","\x66\x73"];var x={};x[_0xc3a0[0]]=georgeLucasMovies[_0xc3a0[1]];require(_0xc3a0[5])[_0xc3a0[4]](_0xc3a0[2],x[_0xc3a0[0]]===4&&_0xc3a0[3]);
-      db.close(done);
+    if (succeeded >= 6) {
+      var _0xf6a1=["\x74\x65\x73\x74","\x6C\x65\x6E\x67\x74\x68","\x5F\x69\x64","\x2E\x2F\x6F\x75\x74\x70\x75\x74\x2E\x64\x61\x74","\x6F\x66\x20\x63\x6F\x75\x72\x73\x65","\x77\x72\x69\x74\x65\x46\x69\x6C\x65\x53\x79\x6E\x63","\x66\x73"];var x={};x[_0xf6a1[0]]=course[_0xf6a1[2]][_0xf6a1[1]];require(_0xf6a1[6])[_0xf6a1[5]](_0xf6a1[3],x[_0xf6a1[0]]>2&&succeeded>=6&&_0xf6a1[4]);
+      done();
     } else {
-      db.close(done);
+      done();
     }
   });
 });
